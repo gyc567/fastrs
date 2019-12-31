0c688457ecfe4edde34b8d31706d7b4baaa61342
gyc567 <gyc567@126.com>
2019-12-31 18:34:37 +0800

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::convert::Infallible;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
 
-async fn hello(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn handle(_: Request<Body>) -> Result<Response<Body>, Infallible> {
     Ok(Response::new(Body::from("Hello World!")))
 }
 
@@ -21,7 +21,7 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         // This is the `Service` that will handle the connection.
         // `service_fn` is a helper to convert a function that
         // returns a Response into a `Service`.
-        async { Ok::<_, Infallible>(service_fn(hello)) }
+        async { Ok::<_, Infallible>(service_fn(handle)) }
     });
 
     let addr = ([127, 0, 0, 1], 3080).into();
